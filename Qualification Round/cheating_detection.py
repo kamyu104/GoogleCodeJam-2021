c19e277e3bddc4e051a70c2a03a55479f38d9b88
Allen Liu <kamyu104@gmail.com>
2021-03-28 05:07:45 +0800

    update

--- Qualification Round/cheating_detection.py
+++ Qualification Round/cheating_detection.py
@@ -3,32 +3,31 @@
 # Google Code Jam 2021 Qualification Round - Problem E. Cheating Detection
 # https://codingcompetitions.withgoogle.com/codejam/round/000000000043580a/00000000006d1155
 #
-# Time:  O(S * Q + SlogS + QlogQ)
+# Time:  O(S * Q + QlogQ)
 # Space: O(S + Q)
 #
 
 def cheating_detection():
     scores = []
-    p_count = [0]*S
     q_count = [0]*Q
     for i in xrange(S):
         scores.append(list(raw_input().strip()))
         for j, c in enumerate(scores[i]):
             if c == '0':
                 continue
-            p_count[i] += 1
             q_count[j] += 1
-    players = sorted(range(S), key=lambda x:p_count[x])
-    questions = sorted(range(Q), key=lambda x:-q_count[x])
+    questions = sorted(range(Q), key=lambda x:q_count[x])
     result, max_score = 0, 0.0
-    for i in players:
-        f = sum_f = 0.0
+    for i in xrange(S):
+        cnt = [0]*2
+        inv = 0.0
         for j in questions:
-            if scores[i][j] == '1':
-                sum_f += f
+            if scores[i][j] == '0':
+                cnt[0] += 1
+                inv += cnt[1]
             else:
-                f += 1
-        score = sum_f/f/(Q-f)
+                cnt[1] += 1
+        score = inv/(1+cnt[0])/(1+cnt[1])
         if score > max_score:
             max_score = score
             result = i
