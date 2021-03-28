0ed064fb23f7eaddd6d560be1a8efbbbf33753cf
Allen Liu <kamyu104@gmail.com>
2021-03-28 09:46:05 +0800

    update

--- Qualification Round/cheating_detection2.py
+++ Qualification Round/cheating_detection2.py
@@ -1,43 +1,41 @@
 # Time:  O(S * Q + SlogS + QlogQ)
 # Space: O(S + Q)
 
-def diff(players, q_ordering, i, j):
-    return abs(sum(players[i][q_ordering[k]] == '0' for k in xrange(int(Q*RATIO))) -
-               sum(players[j][q_ordering[k]] == '0' for k in xrange(int(Q*RATIO))) +
-               sum(players[i][q_ordering[k]] == '0' for k in xrange(Q-int(Q*RATIO), Q)) -
-               sum(players[j][q_ordering[k]] == '0' for k in xrange(Q-int(Q*RATIO), Q)))
+def diff(player1, player2, questions):
+    return abs(sum(player1[questions[i]] == '1' for i in xrange(int(Q*RATIO))) -
+               sum(player2[questions[i]] == '1' for i in xrange(int(Q*RATIO))) +
+               sum(player1[questions[i]] == '1' for i in xrange(Q-int(Q*RATIO), Q)) -
+               sum(player2[questions[i]] == '1' for i in xrange(Q-int(Q*RATIO), Q)))
 
-def f(players, p_ordering, q_ordering, i):
+def f(scores, players, questions, i):
     val = 0.0
     cnt = 0
     if i-1 >= 0:
-        val += diff(players, q_ordering, p_ordering[i-1], p_ordering[i])
+        val += diff(scores[players[i-1]], scores[players[i]], questions)
         cnt += 1
     if i+1 < S:
-        val += diff(players, q_ordering, p_ordering[i], p_ordering[i+1])
+        val += diff(scores[players[i]], scores[players[i+1]], questions)
         cnt += 1
     return val / cnt
 
 def cheating_detection():
-    players = []
+    scores = []
     p_count = [0]*S
     q_count = [0]*Q
     for i in xrange(S):
-        players.append(raw_input().strip())
-        for j, c in enumerate(players[i]):
+        scores.append(raw_input().strip())
+        for j, c in enumerate(scores[i]):
             if c == '0':
                 continue
             p_count[i] += 1
             q_count[j] += 1
-    p_ordering = range(S)
-    p_ordering.sort(key=lambda x:p_count[x])
-    q_ordering = range(Q)
-    q_ordering.sort(key=lambda x:q_count[x])
+    players = sorted(range(S), key=lambda x:p_count[x])
+    questions = sorted(range(Q), key=lambda x:q_count[x])
     result = 0
     for i in xrange(S):
-        if f(players, p_ordering, q_ordering, i) > f(players, p_ordering, q_ordering, result):
+        if f(scores, players, questions, i) > f(scores, players, questions, result):
             result = i
-    return p_ordering[result]+1
+    return players[result]+1
 
 RATIO = 0.05
 S, Q, T, P = 100, 10000, input(), input()
