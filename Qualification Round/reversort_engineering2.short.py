4656904efe132881fb97cbfd5e359c486dd923fc
Allen Liu <kamyu104@gmail.com>
2021-03-29 03:59:17 +0800

    update

---
+++ Qualification Round/reversort_engineering2.short.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2021 kamyu. All rights reserved.
+#
+# Google Code Jam 2021 Qualification Round - Problem C. Reversort Engineering
+# https://codingcompetitions.withgoogle.com/codejam/round/000000000043580a/00000000006d12d7
+#
+# Time:  O(N)
+# Space: O(N)
+#
+
+def reversort_engineering():
+    N, C = map(int, raw_input().strip().split())
+
+    if not (N-1 <= C <= (N+2)*(N-1)//2):
+        return "IMPOSSIBLE"
+    result = [0]*N
+    for i in xrange(N):
+        l = min(C-(N-1-i)+1, N-i)  # greedy
+        C -= l
+        fill = (list(reversed(range(i+1, i+1+l))) + range(i+1+l, N+1)) if l != N-i else [i+1]
+        if i%2 == 0:
+            result[N-1-i//2+1-len(fill):N-1-i//2+1] = fill
+        else:
+            result[i//2:i//2+len(fill)] = fill[::-1]
+        if l != N-i:
+            break
+    return " ".join(map(str, result))
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, reversort_engineering())
