8e8ea45697769be806d8be095fae72715eccecef
Allen Liu <kamyu104@gmail.com>
2021-03-29 03:52:32 +0800

    update

--- Qualification Round/reversort_engineering2.py
+++ Qualification Round/reversort_engineering2.py
@@ -4,8 +4,14 @@
 # https://codingcompetitions.withgoogle.com/codejam/round/000000000043580a/00000000006d12d7
 #
 # Time:  O(N)
-# Space: O(N)
+# Space: O(1)
 #
+
+def reverse(L, i, j):
+    while i < j:
+        L[i], L[j] = L[j], L[i]
+        i += 1
+        j -= 1
 
 def reversort_engineering():
     N, C = map(int, raw_input().strip().split())
@@ -16,13 +22,24 @@ def reversort_engineering():
     for i in xrange(N):
         l = min(C-(N-1-i)+1, N-i)  # greedy
         C -= l
-        fill = (list(reversed(range(i+1, i+1+l))) + range(i+1+l, N+1)) if l != N-i else [i+1]
-        if i%2 == 0:
-            result[N-1-i//2+1-len(fill):N-1-i//2+1] = fill
-        else:
-            result[i//2:i//2+len(fill)] = fill[::-1]
         if l != N-i:
             break
+        if i%2 == 0:
+            result[N-1-i//2] = i+1
+        else:
+            result[i//2] = i+1
+    if i%2 == 0:
+        k = i+1
+        for j in xrange((N-1-i//2+1)-(N-i), N-1-i//2+1):
+            result[j] = k
+            k += 1
+        reverse(result, (N-1-i//2+1)-(N-i), ((N-1-i//2+1)-(N-i))+l-1)
+    else:
+        k = i+1
+        for j in reversed(xrange(i//2, i//2+(N-i))):
+            result[j] = k
+            k += 1
+        reverse(result, (i//2+(N-i)-1)-l+1, i//2+(N-i)-1)
     return " ".join(map(str, result))
 
 for case in xrange(input()):
