9b77ab85a369cdc786e151cc268c12cb4a89da36
Allen Liu <kamyu104@gmail.com>
2021-05-02 07:44:46 +0800

    update

--- Round 1C/double_or_noting.py
+++ Round 1C/double_or_noting.py
@@ -11,6 +11,7 @@ def flip(s):
     return "".join(["01"[c == '0'] for c in s]).lstrip('0') or "0"
 
 def change_count(s):
+    s += '0'  # if s ends with '1', it requires one more "not", which can be easily counted by appending a '0' 
     return sum(int(s[i] != s[i+1]) for i in xrange(len(s)-1))
 
 def double_or_noting():
@@ -19,16 +20,16 @@ def double_or_noting():
     result = float("inf")
     X = 0
     while S != "0":
-        if S == E[:len(S)] and X >= change_count(E[len(S):]+'0'):
+        if S == E[:len(S)] and X >= change_count(E[len(S):]):
             result = min(result, X+len(E[len(S):]))
         S = flip(S)
         X += 1
-    if X >= change_count(E+'0'):
+    if X >= change_count(E):
         result = min(result, X+len(E))
     if E == '0':
         result = min(result, X)
     elif E[0] == '1':
-        cnt = change_count(E[1:]+'0')
+        cnt = change_count(E[1:])
         if cnt == 0:  # if E is one of 1, 10..0
             result = min(result, X+1+(len(E)-1))
         elif cnt == 1:  # if E is one of 110..0, ..., 111..10, 111..1
