a2b9d60104633ec514a8abb78e9a78332eedfeed
Allen Liu <kamyu104@gmail.com>
2021-05-02 06:29:28 +0800

    update

--- Round 1C/double_or_nothing.py
+++ Round 1C/double_or_nothing.py
@@ -19,24 +19,23 @@ def double_or_nothing():
     S, E = raw_input().strip().split()
 
     result = float("inf")
-    prefix = S
     X = 0
-    while prefix != "0":
-        if prefix == E[:len(prefix)] and X >= change_count(E[len(prefix):]+'0'):
-            result = min(result, X+len(E[len(prefix):]))
-        prefix = flip(prefix)
+    while S != "0":
+        if S == E[:len(S)] and X >= change_count(E[len(S):]+'0'):
+            result = min(result, X+len(E[len(S):]))
+        S = flip(S)
         X += 1
     if X >= change_count(E+'0'):
         result = min(result, X+len(E))
     if E == '0':
         result = min(result, X)
     elif E[0] == '1':
-        if len(E) == 1:
+        if len(E) == 1: # case of 1
             result = min(result, X+1)
         else:
-            if E[1:] == '0'*(len(E)-1):  # 10..0
+            if E[1:] == '0'*(len(E)-1):  # case of 10..0
                 result = min(result, X+1+(len(E)-1))
-            elif change_count(E) <= 1:  # 110..0, ..., 111..10, 111..1
+            elif change_count(E) <= 1:  # cases of 110..0, ..., 111..10, 111..1
                 result = min(result, X+1+len(E)+1)
     return "IMPOSSIBLE" if result == float("inf") else result
 
