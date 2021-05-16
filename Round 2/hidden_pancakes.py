2aa958b3c71fffddee4cb46bf6e9f41262804206
Allen Liu <kamyu104@gmail.com>
2021-05-17 00:07:11 +0800

    update

--- Round 2/hidden_pancakes.py
+++ Round 2/hidden_pancakes.py
@@ -7,34 +7,33 @@
 # Space: O(N)
 #
 
-def factorial(n):
-    while len(fact) <= n:  # lazy initialization
-        fact.append(fact[-1]*len(fact) % MOD)
-    return fact[n]
-
-def inverse(n):
+def nCr(n, k):
     while len(inv) <= n:  # lazy initialization
+        fact.append(fact[-1]*len(inv) % MOD)
         inv.append(inv[MOD%len(inv)]*(MOD-MOD//len(inv)) % MOD)  # https://cp-algorithms.com/algebra/module-inverse.html
-    return inv[n]
+        inv_fact.append(inv_fact[-1]*inv[-1] % MOD)
+    return (fact[n]*inv_fact[n-k] % MOD) * inv_fact[k] % MOD
 
 def hidden_pancakes():
     N = input()
     V = map(int, raw_input().strip().split())
     V.append(1)
-    result = factorial(N)
+    result = 1
     stk = []
     for v in V:
         if not (v <= len(stk)+1):
             return 0
         cnt = 0
         while v < len(stk)+1:
-            cnt += stk.pop()
-            result = (result * inverse(cnt)) % MOD
+            result = result * nCr((stk[-1]-1)+cnt, cnt) % MOD
+            cnt += stk[-1]
+            stk.pop()
         stk.append(cnt+1)
     return result
 
 MOD = 10**9+7
 fact = [1, 1]
 inv = [0, 1]
+inv_fact = [1, 1]
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, hidden_pancakes())
