4312c2e1e56e5ec47d3fd3e357382aab105d3320
Allen Liu <kamyu104@gmail.com>
2021-05-16 18:48:35 +0800

    update

---
+++ Round 2/matrygons.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2021 kamyu. All rights reserved.
+#
+# Google Code Jam 2021 Round 2 - Problem B. Matrygons
+# https://codingcompetitions.withgoogle.com/codejam/round/0000000000435915/00000000007dbf06
+#
+# Time:  O(NlogN)
+# Space: O(N)
+#
+
+def matrygons():
+    N = input()
+    return dp[N]
+
+MAX_N = 10**6
+pseudo = [0]*(MAX_N+1)
+dp = [0]*(MAX_N+1)
+for i in xrange(1, MAX_N+1):
+    for j in xrange(2*i, MAX_N+1, i):
+        pseudo[j] = max(pseudo[j], pseudo[i-1]+1)
+for i in xrange(1, MAX_N+1):
+    for j in xrange(3*i, MAX_N+1, i):
+        dp[j] = max(dp[j], pseudo[i-1]+1)
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, matrygons())
