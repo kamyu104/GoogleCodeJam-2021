11ce69daa60707eb240a018c8b0b8b44a7dc297b
Allen Liu <kamyu104@gmail.com>
2021-05-16 22:59:38 +0800

    update

--- Round 2/hidden_pancakes.py
+++ Round 2/hidden_pancakes.py
@@ -7,28 +7,27 @@
 # Space: O(N)
 #
 
-def nCr(n, k):
+def factorial(n):
     while len(inv) <= n:  # lazy initialization
         fact.append(fact[-1]*len(inv) % MOD)
         inv.append(inv[MOD%len(inv)]*(MOD-MOD//len(inv)) % MOD)  # https://cp-algorithms.com/algebra/module-inverse.html
         inv_fact.append(inv_fact[-1]*inv[-1] % MOD)
-    return (fact[n]*inv_fact[n-k] % MOD) * inv_fact[k] % MOD
+    return fact[n]
 
 def hidden_pancakes():
     N = input()
     V = map(int, raw_input().strip().split())
     V.append(1)
-    result = 1
+    result = factorial(N)
     stk = []
     for v in V:
         if not (v <= len(stk)+1):
             return 0
         cnt = 0
         while v < len(stk)+1:
-            result = result * nCr(stk[-1]+cnt, cnt) % MOD
-            cnt += stk[-1]+1
-            stk.pop()
-        stk.append(cnt)
+            cnt += stk.pop()
+            result = (result * inv[cnt]) % MOD
+        stk.append(cnt+1)
     return result
 
 MOD = 10**9+7
