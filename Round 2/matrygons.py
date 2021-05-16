dc49ba212041065a16ca3c9eae8cbb46259a47f9
Allen Liu <kamyu104@gmail.com>
2021-05-16 18:56:49 +0800

    update

--- Round 2/matrygons.py
+++ Round 2/matrygons.py
@@ -15,10 +15,10 @@ def matrygons():
 MAX_N = 10**6
 pseudo = [0]*(MAX_N+1)
 dp = [0]*(MAX_N+1)
-for i in xrange(1, MAX_N+1):
+for i in xrange(1, MAX_N+1):  # time < sum(N/i for i in xrange(1, N)) = O(NlogN)
     for j in xrange(2*i, MAX_N+1, i):
         pseudo[j] = max(pseudo[j], pseudo[i-1]+1)
-for i in xrange(1, MAX_N+1):
+for i in xrange(1, MAX_N+1):  # time < sum(N/i for i in xrange(1, N)) = O(NlogN)
     for j in xrange(3*i, MAX_N+1, i):
         dp[j] = max(dp[j], pseudo[i-1]+1)
 for case in xrange(input()):
