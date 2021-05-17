8e3ea872975bd35ae9eb766d03da7be908e9b408
Allen Liu <kamyu104@gmail.com>
2021-05-17 08:52:39 +0800

    update

---
+++ Round 2/hidden_pancakes2.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2021 kamyu. All rights reserved.
+#
+# Google Code Jam 2021 Round 2 - Problem C. Hidden Pancakes
+# https://codingcompetitions.withgoogle.com/codejam/round/0000000000435915/00000000007dc20c
+#
+# Time:  O(N)
+# Space: O(N)
+#
+
+def factorial(n):
+    while len(fact) <= n:  # lazy initialization
+        fact.append(fact[-1]*len(fact) % MOD)
+    return fact[n]
+
+def inverse(n):
+    while len(inv) <= n:  # lazy initialization
+        inv.append(inv[MOD%len(inv)]*(MOD-MOD//len(inv)) % MOD)  # https://cp-algorithms.com/algebra/module-inverse.html
+    return inv[n]
+
+def hidden_pancakes():
+    N = input()
+    V = map(int, raw_input().strip().split())
+    V.append(1)
+    result = factorial(N)
+    stk = []
+    for v in V:
+        if not (v <= len(stk)+1):
+            return 0
+        cnt = 0
+        while v < len(stk)+1:
+            cnt += stk.pop()
+            result = (result * inverse(cnt)) % MOD
+        stk.append(cnt+1)
+    return result
+
+MOD = 10**9+7
+fact = [1, 1]
+inv = [0, 1]
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, hidden_pancakes())
