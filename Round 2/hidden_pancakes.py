6616e765d5c545d39558c6e9fa0bb08b1c26c08e
Allen Liu <kamyu104@gmail.com>
2021-05-25 12:10:50 +0800

    update

--- Round 2/hidden_pancakes.py
+++ Round 2/hidden_pancakes.py
@@ -19,15 +19,19 @@ def hidden_pancakes():
     V = map(int, raw_input().strip().split())
     V.append(1)
     result = 1
-    stk = []
+    stk = []  # keep the size of each subtrees satisfying v
     for v in V:
-        if not (v <= len(stk)+1):
+        if not (v <= len(stk)+1):  # v - the number of subtrees should be less than or equal to 1
             return 0
         cnt = 0
-        while v < len(stk)+1:
+        while v < len(stk)+1:  # pop groups until v == len(stk)+1
+            # reresent a permutation as a tree,
+            # use the largest pancake of the group as root,
+            # and the size of left subtree is stk[-1]-1, the size of right subtree is cnt.
+            # so the number of permutations is as below:
             result = result * nCr(cnt+(stk[-1]-1), (stk[-1]-1)) % MOD
             cnt += stk.pop()
-        stk.append(cnt+1)
+        stk.append(cnt+1)  # len(stk) == v
     return result
 
 MOD = 10**9+7
--- Round 2/hidden_pancakes2.py
+++ Round 2/hidden_pancakes2.py
@@ -21,13 +21,19 @@ def hidden_pancakes():
     N = input()
     V = map(int, raw_input().strip().split())
     V.append(1)
-    result = factorial(N)
-    stk = []
+    result = factorial(N)  # max number of permutations
+    stk = []  # keep the size of each subtrees satisfying v
     for v in V:
-        if not (v <= len(stk)+1):
+        if not (v <= len(stk)+1):  # v - the number of subtrees should be less than or equal to 1
             return 0
         cnt = 0
-        while v < len(stk)+1:
+        while v < len(stk)+1:  # pop groups until v == len(stk)+1
+            # reresent a permutation as a tree,
+            # use the largest pancake of the group as root,
+            # and the size of left subtree is stk[-1]-1, the size of right subtree is cnt.
+            # since the total size of the current tree is cnt, for each valid permutation, it will produce cnt permutations.
+            # we could inversely get the true number of valid permutations from max number of permutations.
+            # so the number of permutations is as below:
             cnt += stk.pop()
             result = (result * inverse(cnt)) % MOD
         stk.append(cnt+1)
