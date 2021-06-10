86008ffa02466c9c5f8e9c03253148f29e5acf93
Allen Liu <kamyu104@gmail.com>
2021-06-11 00:13:18 +0800

    update

--- Round 3/binary_search_game.py
+++ Round 3/binary_search_game.py
@@ -96,8 +96,8 @@ def binary_search_game():
     for k in xrange(1, len(f)):  # O(N) times
         for mask in xrange(2**len(R)):  # O(2^(2^(L-1))) times
             f[k] = addmod(f[k], count(N, M, L, A, U, decode_mask(R, mask), k))  # Time: O(2^L)
-        f[k] += f[k-1]  # accumulate f
-    return mulmod(lagrange_interpolation(f, M), power(M, len(Z)))  # Time: O(N)
+        f[k] += f[k-1]  # f(x) is a polynomial of x with at most N-degree, thus accumulated f(x) is a polynomial of x with at most (N+1)-degree by Faulhaber's formula
+    return mulmod(lagrange_interpolation(f, M), power(M, len(Z)))  # we can determinate accumulated f(x) by N+2 values of f(x) and compute accumulated f[M]
 
 MOD = 10**9+7
 inv = [0, 1]
