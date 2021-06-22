f6965c3c093c2782804915a9b49ca4d366953d04
Allen Liu <kamyu104@gmail.com>
2021-06-22 15:29:31 +0800

    update

--- Qualification Round/moons_and_umbrellas.py
+++ Qualification Round/moons_and_umbrellas.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2021 kamyu. All rights reserved.
 #
-# Google Code Jam 2021 Qualification Round - Problem B. Moons and Embrellas 
+# Google Code Jam 2021 Qualification Round - Problem B. Moons and Umbrellas
 # https://codingcompetitions.withgoogle.com/codejam/round/000000000043580a/00000000006d1145
 #
 # Time:  O(N)
