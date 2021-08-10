c7f925f60777fc1830e89b3c73ec78f4f9fac88a
kamyu <kamyu104@users.noreply.github.com>
2021-08-10 23:51:07 +0800

    Update ropes.py

--- Virtual World Finals/ropes.py
+++ Virtual World Finals/ropes.py
@@ -8,7 +8,9 @@
 #
 # Usage: python interactive_runner.py python3 testing_tool.py 2 -- python ropes.py
 #
-# push it east solution, 92.0% on average
+# push it east solution
+#   - Z=10, 18322/20000 = 91.610 %
+#   - Z=14, 18251/20000 = 91.255 %
 #
 
 from sys import stdout
@@ -23,6 +25,10 @@ def check_result(A_score, B_score):
     assert(input() == int(A_score > B_score))
 
 def greedy(score_matrix):
+    # given that opposing team choose the play that yields the maximum possible score for this turn.
+    # if there is a tie:
+    #   - they will choose one at random as problem description
+    #   - they will not choose by a counter-greedy strategy (choose as far east as possible), since we can never win by this greedy strategy
     result = None
     best_score = max(imap(max, score_matrix))
     for i in xrange(2*N):
@@ -43,7 +49,7 @@ def ropes():
     score_matrix = [[0 for _ in xrange(2*N)] for _ in xrange(2*N)]
     A_score = B_score = 0
     for k in xrange(N):
-        i, j = greedy(score_matrix) if k else (Z-1, Z-1)
+        i, j = greedy(score_matrix) if k else (Z-1, Z-1)  # tuned by experitments that i=j at the first play is better
         A_score += score_matrix[i][j]
         update(i, j, score_matrix)
         i, j = play(i, j)
@@ -52,7 +58,7 @@ def ropes():
     check_result(A_score, B_score)
 
 NEG_INF = float("-inf")
-Z = 10  # found by experiments
+Z = 10  # tuned by experiments that top 4 win rates are with Z = [9, 10, 11, 14], and the top 1 is with Z = [10]
 T, N, W = map(int, raw_input().strip().split())
 for case in xrange(T):
     ropes()
