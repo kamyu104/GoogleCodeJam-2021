c8fd3d5e6e810cd2eba3f59018d4b4713190030d
Allen Liu <kamyu104@gmail.com>
2021-08-14 20:31:46 +0800

    update

--- Virtual World Finals/infinitree.py
+++ Virtual World Finals/infinitree.py
@@ -231,7 +231,7 @@ def infinitree():
                 p = 0
             continue
         h = cycle_length[c]
-        if h not in M_H_powers:  # sum(distinct h) = N => distinct h at most O(sqrt(N)) times, each Time: O(N^3 * logh + N^3 * log(hi)) => Total Time: O(N^3.5 * logN + (N^3.5 * log(logB) + N^3 * logB)) = O(N^3.5 * logN + N^3 * logB) assumed O(logN) = log(logB)
+        if h not in M_H_powers:  # sum(distinct h) = N => distinct h at most O(sqrt(N)) times, each Time: O(N^3 * logh + N^3 * log(hi)) => Total Time: O(N^3.5 * logN + (N^3.5 * log(logB) + N^3 * logB)) = O(N^3.5 * logN + N^3 * logB) assumed O(N) = O(logB)
             M_H_powers[h], prefix_M_H_powers[h] = build_powers_and_prefix_powers(N, get_M_power_x(N, M_H_powers[1], h, INF), ceil_log2_x(min(h1, h2)), INF)
         vector = [0]*N
         for x in reversed(xrange(h)):  # Time: O(h * N^2 * logN) => Total Time O(N^3 * logN)
@@ -244,7 +244,7 @@ def infinitree():
         while (p*2)*h < min(h1, h2):
             p *= 2
             log_p += 1
-        while p > 1:  # log(p) times => Total Time: O(k cycles * log(p) times * (N^2 * log(delta_h))) = O(N^2 * (logB)^2 + N^3 * log(logB)^2)
+        while p > 1:  # log(p) times => Total Time: O(k cycles * log(p) times * (N^2 * log(delta_h))) = O(N^2 * (logB)^2 + N^3 * log(logB)^2) = O(N^3 * logB) assumed O(N) = O(logB)
             if min(h1, h2) - p*h <= 0:
                 p //= 2
                 log_p -= 1
