39db20b3e5c8e26e604044d41178c997da06dcbb
Allen Liu <kamyu104@gmail.com>
2021-09-24 12:20:50 +0800

    update

--- Round 2/matrygons.py
+++ Round 2/matrygons.py
@@ -19,6 +19,6 @@ for i in xrange(1, MAX_N//2+1):  # time < sum(N/i for i in xrange(1, N)) = O(Nlo
     for j in xrange(2*i, MAX_N+1, i):  # min size of pseudo-polygon could be at least 2
         pseudo[j] = max(pseudo[j], pseudo[i-1]+1)  # pseudo(j) = max(pseudo(i-1)+1 for each i s.t. j%i == 0 and j//i >= 2)
         if j//i >= 3:  # min size of polygon should be at least 3
-            dp[j] = max(dp[j], pseudo[i-1]+1)  # dp(j) = max(pseudo(i-1)+1 or each i s.t. j%i == 0 and j//i >= 3)
+            dp[j] = max(dp[j], pseudo[i-1]+1)  # dp(j) = max(pseudo(i-1)+1 for each i s.t. j%i == 0 and j//i >= 3)
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, matrygons())
